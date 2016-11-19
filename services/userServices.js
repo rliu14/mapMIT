d7f767bd35bd7c334eb4f5e8a2fc161198628bc5
Casey Hong <jhong47@mit.edu>
2016-11-19 16:38:03 -0500

    user services for react

--- services/userServices.js
+++ services/userServices.js
@@ -1,7 +1,45 @@
 const BASE_URL = 'http://localhost:3000/users';
 
-  var request = require('request-promise-native');
+var request = require('request-promise-native');
 
-  export default {
+export default {
+	register : (username, password) => {
+		return request({
+			uri : BASE_URL,
+			method : 'POST',
+			json : true,
+			body : {
+				username : username,
+				password : password
+			}
+		});
+	},
 
-  }
+	login : (username, password) => {
+		return request({
+			uri : BASE_URL + '/login',
+			method : 'POST',
+			body : {
+				username : username,
+				password : password
+			},
+			json : true
+		});
+	},
+
+	getCurrentUser: () => {
+		return request({
+			uri : BASE_URL + '/current',
+			method : 'GET',
+			json : true
+		});
+	},
+
+	logout : () => {
+		return request({
+			uri : BASE_URL + '/logout',
+			method: 'PUT',
+			json : true
+		});
+	}
+}
