e150eada771da3f7e66021837f97d9443d88f797
Dora Tzeng <dtzeng@mit.edu>
2016-11-19 18:09:04 -0500

    updated eventServices

--- models/Event.js
+++ models/Event.js
@@ -5,59 +5,59 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 
 /* Schema to represent the Event model */
 var eventSchema = mongoose.Schema({
-	name: { type: String, required: true },
-	description: { type: String }
-	startTime: { type: Date, required: true },
-	endTime: { type: Date, required: true },
-	roomNumber: { type: int, default: null },
-	location: { type: ObjectId, ref: 'Location'},
-	locationDescription: { type: String },
-	host: { type: String, required: true }
+    name: { type: String, required: true },
+    description: { type: String }
+    startTime: { type: Date, required: true },
+    endTime: { type: Date, required: true },
+    roomNumber: { type: int, default: null },
+    location: { type: ObjectId, ref: 'Location'},
+    locationDescription: { type: String },
+    host: { type: String, required: true }
 });
 
 // TODO: methods to update other fields
 
 eventSchema.statics.deleteEvent = function(eventID, cb) {
-	this.findByIdAndRemove(eventID, function(err, deletedEvent) {
-		if (err) {
-			cb(err, null);
-		} else if (deletedEvent != null) {
-			// event is properly deleted
-			cb(err, true);
-		} else {
-			cb(err, false);
-		};
-	});
+    this.findByIdAndRemove(eventID, function(err, deletedEvent) {
+        if (err) {
+            cb(err, null);
+        } else if (deletedEvent != null) {
+            // event is properly deleted
+            cb(err, true);
+        } else {
+            cb(err, false);
+        };
+    });
 };
 
 eventSchema.statics.findEventByLocation = function(loc, cb) {
-	this.find( { location: loc}, function(err, events) {
-		if (err) {
-			cb(err, null);
-		} else if (events.length > 0) {
-			// found events with specified location
-			cb(err, events);
-		} else {
-			cb(err, null);
-		};
-	});
+    this.find( { location: loc}, function(err, events) {
+        if (err) {
+            cb(err, null);
+        } else if (events.length > 0) {
+            // found events with specified location
+            cb(err, events);
+        } else {
+            cb(err, null);
+        };
+    });
 };
 
 eventSchema.statics.findEventByTime = function(time, cb) {
-	this.find( { startTime: {$lt: time}, endTime: {$gt: time} }, function(err, events) {
-		if (err) {
-			cb(err, null);
-		} else if (events.length > 0) {
-			// found events happening at this time
-			cb(err, events);
-		} else {
-			cb(err, null);
-		};
-	});
+    this.find( { startTime: {$lt: time}, endTime: {$gt: time} }, function(err, events) {
+        if (err) {
+            cb(err, null);
+        } else if (events.length > 0) {
+            // found events happening at this time
+            cb(err, events);
+        } else {
+            cb(err, null);
+        };
+    });
 };
 
 eventSchema.statics.updateEventName = function(updatedName) {
-	this.name = updatedName;
+    this.name = updatedName;
 };
 
 module.exports = mongoose.model('Event', eventSchema)
--- services/eventServices.js
+++ services/eventServices.js
@@ -3,5 +3,46 @@ const BASE_URL = 'http://localhost:3000/events';
   var request = require('request-promise-native');
 
   export default {
+    createEvent : (content) => {
+        return request({
+            uri : BASE_URL,
+            method : 'POST',
+            body : {
+                content: content
+            },
+            json : true
+        });
+    },
 
-  }
+    deleteEvent : (eventID) => {
+        return request({
+            uri : BASE_URL + '/${eventID}',
+            method : 'DELETE'
+        });
+    },
+
+    getEvent : (eventID) => {
+        return request({
+            uri : BASE_URL + '/${eventID}',
+            method : 'GET',
+            json : true
+        });
+    },
+
+    getEventsByLocation : (loc) => {
+        return request({
+            uri : BASE_URL + '/${loc}',
+            method : 'GET',
+            json : true
+        });
+    },
+
+    getEventsByTime : (time) => {
+        return request({
+            uri : BASE_URL + '/${loc}',
+            method: 'GET',
+            json : true
+        });
+    },
+  };
+
