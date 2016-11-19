ea8d450f5b9cac1c1b8d46c4a78994b421d936d0
Casey Hong <jhong47@mit.edu>
2016-11-19 15:40:54 -0500

    changed user model setup for consistency across models

--- models/Users.js
+++ models/Users.js
@@ -1,82 +1,73 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
-var userSchema = mongoose.Schema({
+var userSchema = new mongoose.Schema({
 	username: String,
-	password: String,
+	password: String
 });
 
-var userModel = mongoose.model('User', userSchema);
-
-var Users = (function(userModel) {
-	var that = {};
-
-	that.findUser = function(username, callback) {
-		userModel.findOne({ username : username }, function(err, result) {
-			if (err) callback({ msg : err });
-			if (result !== null) {
-				callback(null, result);
-			} else {
-				callback({ msg : 'No such user!' });
-			}
-		});
-	}
-
-	that.checkPassword = function(username, password, callback) {
-		userModel.findOne({ username : username }, function(err, result) {
-			if (err) callback({ msg: err });
-			if (result !== null && password === result.password) {
-				callback(null, true);
-			} else {
-				callback(null, false);
-			}
-		});
-	}
-
-	that.createUser = function(username, password, callback) {
-		if (username.match('^[a-z0-9_-]{3,16}$') && typeof password === 'string') {
-			this.find({ username : username }, function(err, result) {
-				if (err) callback(err);
-				else if (result.length === 0) {
-					var salt = bcrypt.genSaltSync(10);
-					var hash = bcrypt.hashSync(password, salt);
-					var user = new userModel({
-						username: username,
-						password: hash,
-					});
-					user.save(function(err, result) {
-						if (err) callback(err);
-						else callback(null, { username : username });
-					});
-				} else {
-					callback({ msg : 'User already exists.' });
-				}
-			});
+userSchema.statics.findUser = function(username, callback) {
+	userModel.findOne({ username : username }, function(err, result) {
+		if (err) callback({ msg : err });
+		if (result !== null) {
+			callback(null, result);
 		} else {
-			if (callback) {
-				callback({ msg : 'Invalid username/password.' });
-			}
+			callback({ msg : 'No such user!' });
 		}
-	}
+	});
+};
 
-	that.authUser = function(username, password, callback) {
+userSchema.statics.checkPassword = function(username, password, callback) {
+	userModel.findOne({ username : username }, function(err, result) {
+		if (err) callback({ msg: err });
+		if (result !== null && password === result.password) {
+			callback(null, true);
+		} else {
+			callback(null, false);
+		}
+	});
+};
+
+userSchema.statics.createUser = function(username, password, callback) {
+	if (username.match('^[a-z0-9_-]{3,16}$') && typeof password === 'string') {
 		this.find({ username : username }, function(err, result) {
 			if (err) callback(err);
-			else if (result.length > 0) {
-				if (bcrypt.compareSync(password, result[0].password)) {
-					callback(null, { username : username });
-				} else {
-					callback({ msg : 'Login failed.' });
-				}
+			else if (result.length === 0) {
+				var salt = bcrypt.genSaltSync(10);
+				var hash = bcrypt.hashSync(password, salt);
+				var user = new userModel({
+					username: username,
+					password: hash,
+				});
+				user.save(function(err, result) {
+					if (err) callback(err);
+					else callback(null, { username : username });
+				});
+			} else {
+				callback({ msg : 'User already exists.' });
+			}
+		});
+	} else {
+		if (callback) {
+			callback({ msg : 'Invalid username/password.' });
+		}
+	}
+};
+
+userSchema.statics.authUser = function(username, password, callback) {
+	this.find({ username : username }, function(err, result) {
+		if (err) callback(err);
+		else if (result.length > 0) {
+			if (bcrypt.compareSync(password, result[0].password)) {
+				callback(null, { username : username });
 			} else {
 				callback({ msg : 'Login failed.' });
 			}
-		});
-	}
+		} else {
+			callback({ msg : 'Login failed.' });
+		}
+	});
+};
 
-	Object.freeze(that);
-	return that;
-
-})(userModel);
-
+var Users = mongoose.model('User', userSchema)
 module.exports = Users;
