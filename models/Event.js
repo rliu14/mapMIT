a20f9db91b4d98e3ec941a5003a49d6eb7fa195c
Dora Tzeng <dtzeng@mit.edu>
2016-11-19 16:15:15 -0500

    added Event model methods

--- models/Event.js
+++ models/Event.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 /* Schema to represent the Event model */
-var EventSchema = mongoose.Schema({
+var eventSchema = mongoose.Schema({
 	name: { type: String, required: true },
 	description: { type: String }
 	startTime: { type: Date, required: true },
@@ -14,5 +14,50 @@ var EventSchema = mongoose.Schema({
 	locationDescription: { type: String },
 	host: { type: String, required: true }
 });
+
+// TODO: methods to update other fields
+
+eventSchema.statics.deleteEvent = function(eventID, cb) {
+	this.findByIdAndRemove(eventID, function(err, deletedEvent) {
+		if (err) {
+			cb(err, null);
+		} else if (deletedEvent != null) {
+			// event is properly deleted
+			cb(err, true);
+		} else {
+			cb(err, false);
+		};
+	});
+};
+
+eventSchema.statics.findEventByLocation = function(loc, cb) {
+	this.find( { location: loc}, function(err, events) {
+		if (err) {
+			cb(err, null);
+		} else if (events.length > 0) {
+			// found events with specified location
+			cb(err, events);
+		} else {
+			cb(err, null);
+		};
+	});
+};
+
+eventSchema.statics.findEventByTime = function(time, cb) {
+	this.find( { startTime: {$lt: time}, endTime: {$gt: time} }, function(err, events) {
+		if (err) {
+			cb(err, null);
+		} else if (events.length > 0) {
+			// found events happening at this time
+			cb(err, events);
+		} else {
+			cb(err, null);
+		};
+	});
+};
+
+eventSchema.statics.updateEventName = function(updatedName) {
+	this.name = updatedName;
+};
 
 module.exports = mongoose.model('Event', eventSchema)
