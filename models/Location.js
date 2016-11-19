657d9b9f6c7f2111cd6678037cdfe19a447a97bd
Rena Liu <rliu14@mit.edu>
2016-11-19 15:20:24 -0500

    Added static methods for finding by location name

--- models/Location.js
+++ models/Location.js
@@ -3,14 +3,28 @@ var mongoose = require("mongoose");
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 /**
- * The schema for a location. A location requires a locationName, coordinates.
+ * The schema for a location. A location requires a locationName and coordinates.
  */
  var location = mongoose.Schema({
  	locationName: { type: String, required: true, unique: true },
  	coordinateX: { type: int, required: true },
  	coordinateY: { type: int, required: true },
- 	isBuilding: { type: boolean },
- 	roomNumber: {type: int }
+ 	// isBuilding: { type: boolean },
+ 	// roomNumber: {type: int }
  });
+
+ /** 
+  * Finds a location given the locationName. Callback on the location
+  * matching the locationName, or null if an error occurred.
+  */
+ location.statics.findLocation = function(locationName, callback) {
+ 	this.where({'locationName': locationName}).findOne(function (err, foundLocation) {
+ 		if (err) {
+ 			callback(null);
+ 		} else {
+ 			callback(foundLocation);
+ 		}
+ 	});
+ }
 
 module.exports = mongoose.model("Location", location);
