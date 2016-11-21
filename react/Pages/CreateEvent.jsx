9679feadaa87cf413360dbf4b8490d023e7dfc56
Dora Tzeng <dtzeng@mit.edu>
2016-11-20 21:38:07 -0500

    updated createEvent page and event routes

--- react/Pages/CreateEvent.jsx
+++ react/Pages/CreateEvent.jsx
@@ -6,11 +6,94 @@ class CreateEvent extends Component {
 		super(props);
 		this.defaultProps = {
 		}
+		this.updateEventName = this.updateEventName.bind(this);
+		this.updateRoomNumber = this.updateRoomNumber.bind(this);
+		this.updateEventDescription = this.updateEventDescription.bind(this);
+		this.updateLocationDescription = this.updateLocationDescription.bind(this);
+		this.updateHost = this.updateHost.bind(this);
+		this.state = {
+			eventName: '',
+			eventDate: '',
+			startTime: '',
+			endTime: '',
+			roomNumber: '',
+			eventDescription: '',
+			location: '',
+			locationDescription: '',
+			host: ''
+		}
+	};
+
+	updateEventName(event) {
+		this.setState({
+			eventName: event.target.value
+		});
+	}
+
+	// NEED UPDATE EVENTDATE
+	// NEED UPDATE START TIME , END TIME
+
+	updateRoomNumber(event) {
+		this.setState({
+			roomNumber: event.target.value
+		});
+	}
+
+	updateEventDescription(event) {
+		this.setState({
+			eventDescription: event.target.value
+		});
+	}
+
+	// NEED UPDATE EVENT LOCATION
+
+	updateLocationDescription(event) {
+		this.setState({
+			locationDescription: event.target.value
+		});
+	}
+
+	updateHost(event) {
+		this.setState({
+			host: event.target.value
+		});
 	}
 
 	render() {
 	  	return ( 
-	  		<div>Create Event!</div>
+	  		<div>
+		  		<div className="header">
+		  			<h1>Create an Event!</h1>
+		  		</div>
+		  		<div className="input-group">
+		  			
+		  			<span>Event Name* </span> 
+		  			<input type="text" className="form-control" value={this.state.eventName} onChange={this.updateEventName}></input> <br/>
+
+		  			<span>Date* </span> 
+		  			<br/>
+
+		  			<span>Time* </span> 
+		  			<input type="text" className="form-control" placeholder="Start"></input>
+		  			<span> - </span>
+		  			<input type="text" className="form-control" placeholder="End"></input> <br/>
+
+		  			<span>Room Number</span> 
+		  			<input type="text" className="form-control" value={this.state.roomNumber} onChange={this.updateRoomNumber}></input> <br/>
+
+		  			<span>Event Description </span> 
+		  			<input type="text" className="form-control" value={this.eventDescription} onChange={this.updateEventDescription}></input> <br/>
+
+		  			<span>Select Location* </span> 
+		  			<br/>
+
+		  			<span>Location Description </span> 
+		  			<input type="text" className="form-control" value={this.locationDescription} onChange={this.updateLocationDescription}></input> <br/>
+
+		  			<span>Host* </span> 
+		  			<input type="text" className="form-control" value={this.host} onChange={this.updateHost}></input> <br/>
+		  		</div>
+	  		</div>
 	  	)
 	}
 }
--- routes/events.js
+++ routes/events.js
@@ -5,11 +5,36 @@ var Event = require('../models/Event');
 
 // create event
 router.post('/', function(req, res) {
-
+	Event.create({
+		name: req.body.content.eventName,
+		description: req.body.content.eventDescription,
+	    startTime: req.body.content.startTime,
+	    endTime: req.body.content.endTime,
+	    roomNumber: req.body.content.roomNumber,
+	    location: req.body.content.location,
+	    locationDescription: req.body.content.locationDescription,
+	    host: req.body.content.host
+	}, function(err, createdEvent) {
+		if(err) {
+			if(err.msg) {
+				utils.sendErrorResponse(res, 400, err.msg);
+			} else {
+				utils.sendErrorResponse(res, 500, 'An unknown error occurred.');
+			}
+		} else {
+			utils.sendSuccessResponse(res, createdEvent);
+		}
+	});
 });
 
 router.get('/:eventID', function(req, res) {
-
+	Event.findEventByID(req.params.eventID, function(err, foundEvent) {
+		if(err) {
+			utils.sendErrorResponse(res, 404, 'No such event.');
+		} else {
+			utils.sendSuccessResponse(res, { foundEvent: foundEvent });
+		};
+	});
 });
 
 router.get('/location/:loc', function(req, res) {
