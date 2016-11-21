e1c6c6266ab7ccead928f2a92647848031643cfd
Dora Tzeng <dtzeng@mit.edu>
2016-11-20 20:40:51 -0500

    outline for event routes

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var utils = require('../utils/utils');
+var User = require('../models/Event');
 
+// create event
+router.post('/', function(req, res) {
 
+});
 
+router.get('/:eventID', function(req, res) {
+
+});
+
+router.get('/location/:loc', function(req, res) {
+
+});
+
+router.get('/time/:time', function(req, res) {
+
+});
+
+router.delete('/:eventID', function(req, res) {
+
+});
 
 
 module.exports = router;
