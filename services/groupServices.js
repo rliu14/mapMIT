af6f1982c9e203499d49721b5231191e178c01a1
Elysa Kohrs <elysakohrs@gmail.com>
2016-12-01 09:59:46 -0500

    group services

--- services/groupServices.js
+++ services/groupServices.js
@@ -1,7 +1,55 @@
 const BASE_URL = 'http://localhost:3000/groups';
 
-  var request = require('request-promise-native');
+var request = require('request-promise-native');
 
-  export default {
+export default {
+    createGroup : (content) => {
+        return request({
+            uri : BASE_URL,
+            method : 'POST',
+            body : {
+                content: content
+            },
+            json : true
+        });
+    },
 
-  }
+    addMemberToGroup : (groupId, content) => {
+        return request({
+            uri : BASE_URL + `/add/${groupId}`,
+            method : 'PUT',
+            body : {
+                content : content
+            },
+            json : true
+        });
+    },
+
+    removeMemberFromGroup : (groupId, content) => {
+        return request({
+            uri : BASE_URL + `/remove/${groupId}`,
+            method : 'PUT',
+            body : {
+                content : content
+            },
+            json : true
+        });
+    },
+
+    getGroupsByCreator : (creator) => {
+        return request({
+            uri : BASE_URL + `/creator/${creator}`,
+            method: 'GET',
+            json : true
+        });
+    },
+
+    getEventsWithMember : (loc) => {
+        return request({
+            uri : BASE_URL + `/member/${member}`,
+            method : 'GET',
+            json : true
+        });
+    },
+
+}
