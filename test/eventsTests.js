e08177554c58f02d821da89a87484816fad8c4cb
Dora Tzeng <dtzeng@mit.edu>
2016-12-04 17:02:46 -0500

    finished event tests!!!

--- test/eventsTests.js
+++ test/eventsTests.js
@@ -60,27 +60,140 @@ describe("App", function() {
         });
 
         it("should delete an event from the database", function(done) {
-            done();
+            var content = {
+                name: 'Test Event',
+                startTime: Date.now(),
+                endTime: Date.now(),
+                room: '32-123',
+                description: 'test description',
+                locationDescription: 'test location description',
+                creator: 'user_a',
+                location: 'maseeh'
+            };
+            Event.createEvent(content, function(err, mEvent) {
+                var eventID = mEvent._id;
+                Event.deleteEvent(eventID, function(err, deletedEvent) {
+                    Event.count(function(err, count){
+                        assert.equal(count, 0);
+                        done();
+                    })
+                })
+            });
         });
 
         it("should find events by the creator", function(done) {
-            done();  
+            var content = {
+                name: 'Test Event',
+                startTime: Date.now(),
+                endTime: Date.now(),
+                room: '32-123',
+                description: 'test description',
+                locationDescription: 'test location description',
+                creator: 'user_a',
+                location: 'maseeh'
+            };
+            Event.createEvent(content, function(err, mEvent) {
+                Event.findEventsByCreator('user_a', function(err, events) {
+                    assert.equal(events.length, 1);
+                    assert.deepEqual(events[0]._id, mEvent._id);
+                    assert.equal(events[0].name, 'Test Event');
+                    done();
+                })
+            }); 
         });
 
         it("should find an event by an id", function(done) {
-            done();  
+            var content = {
+                name: 'Test Event',
+                startTime: Date.now(),
+                endTime: Date.now(),
+                room: '32-123',
+                description: 'test description',
+                locationDescription: 'test location description',
+                creator: 'user_a',
+                location: 'maseeh'
+            };
+            Event.createEvent(content, function(err, mEvent) {
+                var eventID = mEvent._id;
+                Event.findEventByID(eventID, function(err, foundEvent) {
+                    assert.deepEqual(mEvent._id, foundEvent._id);
+                    done();
+                })
+            });
         });
 
         it("should find events by location", function(done) {
-            done();  
+            var content = {
+                name: 'Test Event',
+                startTime: Date.now(),
+                endTime: Date.now(),
+                room: '32-123',
+                description: 'test description',
+                locationDescription: 'test location description',
+                creator: 'user_a',
+                location: 'maseeh'
+            };
+            Event.createEvent(content, function(err, mEvent) {
+                Event.findEventsByLocation('maseeh', function(err, events) {
+                    assert.equal(events.length, 1);
+                    assert.deepEqual(mEvent._id, events[0]._id);
+                    done();
+                })
+            }); 
         });
 
         it("should find events by time", function(done) {
-            done();  
+            var content = {
+                name: 'Test Event',
+                startTime: new Date('2014-01'),
+                endTime: new Date('2016-01'),
+                room: '32-123',
+                description: 'test description',
+                locationDescription: 'test location description',
+                creator: 'user_a',
+                location: 'maseeh'
+            };
+            Event.createEvent(content, function(err, mEvent) {
+                var time = new Date('2015-01');
+                var startTime = new Date('2014-01');
+                Event.findEventsByTime(time, function(err, events) {
+                    assert.equal(events.length, 1);
+                    assert.deepEqual(mEvent.startTime, startTime);
+                    done();
+                })
+            });
         });
 
         it("should find a specific event by id, then update that event", function(done) {
-            done();  
+            var content = {
+                name: 'Test Event',
+                startTime: Date.now(),
+                endTime: Date.now(),
+                room: '32-123',
+                description: 'test description',
+                locationDescription: 'test location description',
+                creator: 'user_a',
+                location: 'maseeh'
+            };
+            Event.createEvent(content, function(err, mEvent) {
+                var eventID = mEvent._id;
+                var newContent = {
+                    name: 'Updated Event',
+                    startTime: Date.now(),
+                    endTime: Date.now(),
+                    room: '26-100',
+                    description: 'test description',
+                    locationDescription: 'test location description',
+                    creator: 'user_a',
+                    location: 'maseeh'
+                };
+                Event.findAndUpdateEvent(eventID, newContent, function(err, updatedEvent) {
+                    assert.deepEqual(updatedEvent._id, mEvent._id);
+                    assert.equal(updatedEvent.name, 'Updated Event');
+                    assert.equal(updatedEvent.room, '26-100');
+                    done();
+                });
+            });
         });
     });
 
