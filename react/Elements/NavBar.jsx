9f21e9c61677f0e61a9149828c86d647daa837bf
Elysa Kohrs <elysakohrs@gmail.com>
2016-12-07 22:54:22 -0500

    navbar redesign part 1

--- react/Elements/NavBar.jsx
+++ react/Elements/NavBar.jsx
@@ -15,7 +15,7 @@ class NavBar extends Component {
 
 	render() {
 		var currentUserItem = (this.props.currentUser === undefined || this.props.currentUser === 'Not Logged In') ? null : (
-			<p>Welcome, {this.props.currentUser}</p>
+			<p>Logged in as: {this.props.currentUser}</p>
 		);
 
 		var myEventsItem = this.props.currentUser === undefined ? null : (
@@ -33,34 +33,33 @@ class NavBar extends Component {
 
 		return (
 			<nav className = 'navbar'>
-				<div className = 'container'>
-					<ul>
-						<div id="home-link">
-							<li>
-								<IndexLink to = '/' className = 'homepage'>MapMIT</IndexLink>
-							</li>
-						</div>
-						<div id="welcome-user">
-							<li>
-								{ currentUserItem }
-							</li>
-						</div>
-						<div id="user-events">
-							<li>
-								{ myEventsItem }
-							</li>
-						</div>
-						<div id="user-groups">
-							<li>
-								{ myGroupsItem }
-							</li>
-						</div>
-						<div id="logout">
-							<li>
-								{ logoutItem }
-							</li>
-						</div>
-					</ul>
+
+			<div id="navbar-left">
+				<div id="home-link" className="navbar-item right-bar">
+					<IndexLink to = '/' className = 'homepage'>Home</IndexLink>
+				</div>
+				<div id="user-events" className="navbar-item right-bar">
+					{ myEventsItem }
+				</div>
+				<div id="user-groups" className="navbar-item">
+					{ myGroupsItem }
+				</div>	
+				</div>	
+
+			<div id="navbar-center">
+
+				<div className="navbar-item">
+					<img className="navbar-logo-img" src={require('../../public/img/logo_white.png')}/>
+				</div>
+				</div>
+
+<div id="navbar-right">
+				<div id="welcome-user" className="navbar-item">
+					{ currentUserItem }
+				</div>
+				<div id="logout" className="navbar-item">
+					{ logoutItem }
+				</div>
 				</div>
 			</nav>
 		)
