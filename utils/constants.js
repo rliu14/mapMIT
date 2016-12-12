61ad084f2588f4a475ba4897dfe023e2fe37c18a
Dora Tzeng <dtzeng@mit.edu>
2016-12-11 22:30:12 -0500

    group routes restful! and more documentation

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 // Constants for use throughout app.
-// const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://mapmit.herokuapp.com' : 'http://localhost:3000';
-const BASE_URL = 'http://mapmit.herokuapp.com';
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://mapmit.herokuapp.com' : 'http://localhost:3000';
+// const BASE_URL = 'http://mapmit.herokuapp.com';
 
 module.exports = {
     BASE_URL: BASE_URL
